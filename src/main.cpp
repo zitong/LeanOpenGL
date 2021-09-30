0e90c8b985f061db245645f603a71e3c06cf3882
lichenyang <zitongcheng@qq.com>
2021-09-30 18:28:27 +0800

    修改include

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
 #include<stdio.h>
 #include<iostream>
-
-extern "C" {
-    #include <glad/glad.h>
-    #include <GLFW/glfw3.h>
-}
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
