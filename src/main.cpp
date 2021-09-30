734abdfafa21d063ea4cf95da733fa43f7d49fa1
lichenyang <zitongcheng@qq.com>
2021-09-30 19:05:31 +0800

    2.Hello Triangle

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,74 @@ void processInput(GLFWwindow *window)
         glfwSetWindowShouldClose(window, true);
 }
 
+void useShader()
+{   
+    //创建顶点着色器
+    const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    
+    //创建片元着色器
+    const char *fragmentShaderSource = R"(#version 330 core
+    out vec4 FragColor;
+    void main()
+    {
+        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    }
+    )";
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    //创建着色器程序并连接定点着色器和片元着色器
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    
+    //设置当前使用的着色器程序
+    glUseProgram(shaderProgram);
+    
+    //回收资源
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+}
+
+void render()
+{
+    //创建VAO
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    //创建VBO
+    float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+    };
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);  
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    //指定渲染shader
+    useShader();
+    //指定渲染的VAO
+    glBindVertexArray(VAO);
+    //渲染图形
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
 int main(){
     printf("hello opengl\n");
     glfwInit();
@@ -40,8 +108,9 @@ int main(){
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
-        glClearColor(1, 0, 0, 1);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        render();
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
